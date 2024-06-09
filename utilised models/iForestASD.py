1f7dd5def95659d2cbf1c985e8874dec99f3fab4
apfelinmv <133810326+apfelinmv@users.noreply.github.com>
2024-06-09 14:16:22 +0300

    Update iForestASD.py

--- utilised models/iForestASD.py
+++ utilised models/iForestASD.py
@@ -1,43 +1,44 @@
 import numpy as np
-
+import time
 
 # Z - input data, L - number of trees, N - subsampling size in window, E - current anomaly detector, u - predifined anomaly detector
 
 class IForestASD:
-    def __init__(self, window_size=256, tree_num=100,  u=0.5):
+    def __init__(self, window_size=256, tree_num=100, u=0.5, sample_size=256):
         self.window_size = window_size
+        self.sample_size = sample_size
         self.tree_num = tree_num
         self.u = u
 
         self.window = None
         self.previous_window = None
 
-        self.all_pred = [] #все аномалии из датасета, чтобы  картинка была
-        self.all_anomalies = [] #все аномалии из датасета, чтобы  картинка была
+        self.all_pred = []  # все аномалии из датасета, чтобы  картинка была
+        self.all_anomalies = []  # все аномалии из датасета, чтобы  картинка была
 
         self.curr_row_num = 0
+
+    def train(self, X_train: np.ndarray): # этого не написано в бумажке
+        self.iforest_window = IForest(self.window_size, self.tree_num)
+        self.iforest_window.fit(X_train)
 
     def first_fit(self, X: np.ndarray):
         total_row_num = X.shape[0]
 
         self.window = np.empty((0, X.shape[1]))
         if self.curr_row_num == 0:
-            self.iforest_window = IForest(self.window_size, self.tree_num)
             for i in range(self.window_size):
                 row = np.reshape(X[i], (1, len(X[i])))
-                # не понятно как этот момент будет ирл работать на постоянном потоке, но думаю лего исправить
                 self.window = np.append(self.window, row, axis=0)
                 self.curr_row_num += 1
-            self.iforest_window.fit(self.window)
 
 
         for i in range(self.curr_row_num, total_row_num):
             self.second_fit(X[i])
 
-
     def second_fit(self, X: np.ndarray):
         X = np.reshape(X, (1, len(X)))
-        if self.curr_row_num % self.window_size == 0: #это на заполнение
+        if self.curr_row_num % self.window_size == 0:  # это на заполнение
             self.previous_window = self.window
             self.window = X
 
@@ -46,13 +47,14 @@ class IForestASD:
 
             if score_count > self.u:
                 self.iforest_window.fit(self.previous_window)
+                print("model changed")
 
             window_anomaly_scores = self.print_anomaly_scores(self.previous_window)
             window_predictions = self.print_predictions(self.previous_window)
             self.all_predicts(window_predictions)
             self.all_anomaly_scores(window_anomaly_scores)
-            #print("Window Anomaly Scores:", window_anomaly_scores) #для каждого окна принтит аномали скоры
-            print("Window Predictions:", window_predictions) #для каждого окна принтит предикты
+            # print("Window Anomaly Scores:", window_anomaly_scores) #для каждого окна принтит аномали скоры
+            print("Window Predictions:", window_predictions)  # для каждого окна принтит предикты
 
         else:
             self.window = np.concatenate((self.window, X))
@@ -67,17 +69,16 @@ class IForestASD:
         window_predictions = self.iforest_window.predict(previous_window, self.u)
         return window_predictions
 
-    def all_predicts(self, window_predicts): #все аномалии из датасета, чтобы  картинка была
+    def all_predicts(self, window_predicts):  # все аномалии из датасета, чтобы  картинка была
         self.all_pred.extend(window_predicts)
         return self.all_pred
-    
-    def all_anomaly_scores(self, window_anomalies): #все аномалии из датасета, чтобы  картинка была
+
+    def all_anomaly_scores(self, window_anomalies):  # все аномалии из датасета, чтобы  картинка была
         self.all_anomalies.extend(window_anomalies)
         return self.all_anomalies
 
 
-
-#part 2 iforest model
+# part 2 iforest model
 
 def c(n):
     if n > 2:
@@ -128,7 +129,7 @@ class ITree:
 
 
 class IForest:
-    def __init__(self, sample_size, tree_num):   #psi = 256, t = 100 default
+    def __init__(self, sample_size, tree_num):  # psi = 256, t = 100 default
         self.sample_size = sample_size
         self.tree_num = tree_num
         self.forest = []
@@ -153,7 +154,6 @@ class IForest:
         paths = np.array(allPL)
         return np.mean(paths, axis=1)
 
-
     def anomaly_score(self, X: np.ndarray):
         avg_path_lengths = self.path_length(X)
         scores = []
@@ -169,7 +169,6 @@ class IForest:
 
 
 def PathLength(x, T, e):
-
     if isinstance(T, ExNode):
         return e + c(T.size)
 
@@ -180,6 +179,26 @@ def PathLength(x, T, e):
         return PathLength(x, T.right, e + 1)
 
 
+import numpy as np
+from sklearn.metrics import f1_score, roc_auc_score, roc_curve
+import pandas as pd
+# Import your classes here
+import matplotlib.pyplot as plt
 
 
+
+
+def plot_roc_curve(y_true, y_pred, title, processing_time):
+    fpr, tpr, _ = roc_curve(y_true, y_pred)
+    auc = roc_auc_score(y_true, y_pred)
+    plt.figure(figsize=(8, 6))
+    plt.plot(fpr, tpr, label=f'ROC Curve (AUC = {auc:.4f})')
+    plt.plot([0, 1], [0, 1], 'k--', label='Random Guess')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(title)
+    plt.legend(loc='best')
+    if processing_time is not None:
+        plt.text(0.6, 0.2, f'Processing Time: {processing_time:.2f} sec', fontsize=12)
+    plt.show()
 
